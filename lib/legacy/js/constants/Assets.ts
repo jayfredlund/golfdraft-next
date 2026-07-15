3c5cfdfe47fafb8746841f4a971c8955bca766e5
Jay Fredlund <jayfredlund@gmail.com>
2026-07-15 13:42:05 -0700

    updates sounds

--- lib/legacy/js/constants/Assets.ts
+++ lib/legacy/js/constants/Assets.ts
@@ -1,10 +1,10 @@
 const assets = {
-  MY_TURN_SOUND: '/sounds/airplane_chime.wav',
-  PICK_MADE_SOUND: '/sounds/bleep1.wav',
-  NEW_CHAT_MESSAGE_SOUND: '/sounds/tone1.wav',
+  MY_TURN_SOUND: '/public/sounds/three-note-doorbell.mp3',
+  PICK_MADE_SOUND: '/public/sounds/ball-drop-in-cup.wav',
+  NEW_CHAT_MESSAGE_SOUND: '/public/sounds/soft-notification.mp3',
 
   // TODO: Pick new sounds
-  PICK_WARNING_SOUND: '/sounds/bleep1.wav',
+  PICK_WARNING_SOUND: '/public/sounds/golf-swing-hit.wav',
 } as const;
 
 export default assets;
