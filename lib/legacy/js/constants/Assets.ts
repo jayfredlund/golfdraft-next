f82c7e91ba93df1eae9fd6682aac05f11335943e
Jay Fredlund <jayfredlund@gmail.com>
2026-07-15 14:29:52 -0700

    updates sound paths for nextjs

--- lib/legacy/js/constants/Assets.ts
+++ lib/legacy/js/constants/Assets.ts
@@ -1,10 +1,10 @@
 const assets = {
-  MY_TURN_SOUND: '/public/sounds/three-note-doorbell.mp3',
-  PICK_MADE_SOUND: '/public/sounds/ball-drop-in-cup.wav',
-  NEW_CHAT_MESSAGE_SOUND: '/public/sounds/soft-notification.mp3',
+  MY_TURN_SOUND: '/sounds/three-note-doorbell.mp3',
+  PICK_MADE_SOUND: '/sounds/ball-drop-in-cup.wav',
+  NEW_CHAT_MESSAGE_SOUND: '/sounds/soft-notification.mp3',
 
   // TODO: Pick new sounds
-  PICK_WARNING_SOUND: '/public/sounds/golf-swing-hit.wav',
+  PICK_WARNING_SOUND: '/sounds/golf-swing-hit.wav',
 } as const;
 
 export default assets;
