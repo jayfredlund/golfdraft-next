b28bfb4a9c71c33e829ff5a1b9a2449ea39460c3
Jay Fredlund <jayfredlund@gmail.com>
2026-07-15 17:06:29 -0700

    updates warning sounds to occur at 2min, then every 30 seconds

--- lib/legacy/js/components/DraftClock.tsx
+++ lib/legacy/js/components/DraftClock.tsx
@@ -1,12 +1,11 @@
 import moment from 'moment';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { isPendingDraftPick, useDraftPicks } from '../../../data/draft';
 import Assets from '../constants/Assets';
 
 const WARNING_TIME = 1000 * 60 * 2;
 const OVERTIME = 1000 * 60 * 3;
-const FINAL_COUNTDOWN_THRESHOLD = 1000 * 15;
-const WARNING_SOUND_INTERVAL_SECONDS = 10;
+const WARNING_SOUND_INTERVAL_MILLIS = 1000 * 30;
 
 let pickWarningSound: HTMLAudioElement | undefined = undefined;
 try {
@@ -22,7 +21,7 @@ const DraftClock: React.FC<{
   disableClock?: boolean;
 }> = ({ isMyPick, disableClock = false }) => {
   const [totalMillis, setTotalMillis] = useState<number | undefined>(undefined);
-  const totalSeconds = (totalMillis ?? 0) / 1000;
+  const lastWarningSoundBucketRef = useRef<number | undefined>(undefined);
 
   const { data: draftPicks } = useDraftPicks();
   const prevPickIndex = (draftPicks?.findIndex(isPendingDraftPick) ?? -1) - 1;
@@ -43,24 +42,31 @@ const DraftClock: React.FC<{
     };
   }, [prevPickEpochMillis]);
 
-  const isInFinalCountdownThreshold = (totalMillis ?? 0) > FINAL_COUNTDOWN_THRESHOLD;
   useEffect(() => {
-    if (!isMyPick || !isInFinalCountdownThreshold) {
+    if (!isMyPick) {
+      lastWarningSoundBucketRef.current = undefined;
       return;
     }
 
-    const id = setInterval(() => {
-      try {
-        pickWarningSound?.play();
-      } catch (e) {
-        // noop
-      }
-    }, WARNING_SOUND_INTERVAL_SECONDS * 1000);
+    if ((totalMillis ?? 0) < WARNING_TIME) {
+      return;
+    }
 
-    return () => {
-      clearInterval(id);
-    };
-  }, [isMyPick, isInFinalCountdownThreshold]);
+    const warningElapsedMillis = (totalMillis ?? 0) - WARNING_TIME;
+    const warningBucket = Math.floor(warningElapsedMillis / WARNING_SOUND_INTERVAL_MILLIS);
+
+    if (lastWarningSoundBucketRef.current === warningBucket) {
+      return;
+    }
+
+    lastWarningSoundBucketRef.current = warningBucket;
+
+    try {
+      pickWarningSound?.play();
+    } catch (e) {
+      // noop
+    }
+  }, [isMyPick, totalMillis]);
 
   if (disableClock) {
     return (
